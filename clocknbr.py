fedc56c34be48fcf18821623de236760d85984c0
Natasha <ichanska.natalia@gmail.com>
2025-03-30 18:52:57 +0300

    все готово)

--- clocknbr.py
+++ clocknbr.py
@@ -3,12 +3,32 @@ import math
 import datetime
 import turtle
 
-class ClockNumber:
-    def __init__(self, number):
-        self.number = number
 
-    def __str__(self):
-        return str(self.number)
+class Watch:
+    def __init__(self):
+        self.current_time = datetime.datetime.now()
+
+    def update_time(self):
+        self.current_time = datetime.datetime.now()
+
+
+class ClockFace:
+    def __init__(self):
+        self.numbers = [i for i in range(1, 13)]
+
+    def draw(self):
+        turtle.penup()
+        turtle.goto(0, -120)
+        turtle.pendown()
+        turtle.circle(120)
+        for i, num in enumerate(self.numbers):
+            x = 100 * math.sin(math.radians(i * 30))
+            y = 100 * math.cos(math.radians(i * 30))
+            turtle.penup()
+            turtle.goto(x, y)
+            turtle.pendown()
+            turtle.write(str(num), align="center", font=("Arial", 14, "bold"))
+
 
 class ClockHand:
     def __init__(self, type_):
@@ -23,45 +43,28 @@ class ClockHand:
         elif self.type_ == 'second':
             self.angle = time_value * 6
 
-    def __str__(self):
-        return f"{self.type_.capitalize()} hand at {self.angle} degrees"
 
-class ClockFace:
+class AnalogWatch(Watch):
     def __init__(self):
-        self.numbers = [ClockNumber(i) for i in range(1, 13)]
-
-    def draw(self):
-        turtle.penup()
-        turtle.goto(0, -120)
-        turtle.pendown()
-        turtle.circle(120)
-        for i, num in enumerate(self.numbers):
-            x = 100 * math.sin(math.radians(i * 30))
-            y = 100 * math.cos(math.radians(i * 30))
-            turtle.penup()
-            turtle.goto(x, y)
-            turtle.write(str(num), align="center", font=("Arial", 14, "bold"))
-
-class Clock:
-    def __init__(self):
+        super().__init__()
         self.face = ClockFace()
         self.hour_hand = ClockHand('hour')
         self.minute_hand = ClockHand('minute')
         self.second_hand = ClockHand('second')
-        self.face.draw()  # Малюємо циферблат один раз
+        self.face.draw()
         self.hands_drawer = turtle.Turtle()
         self.hands_drawer.hideturtle()
 
     def update(self):
-        now = datetime.datetime.now()
-        self.hour_hand.update(now.hour)
-        self.minute_hand.update(now.minute)
-        self.second_hand.update(now.second)
+        self.update_time()
+        self.hour_hand.update(self.current_time.hour)
+        self.minute_hand.update(self.current_time.minute)
+        self.second_hand.update(self.current_time.second)
         self.redraw_hands()
         turtle.ontimer(self.update, 1000)
 
     def redraw_hands(self):
-        self.hands_drawer.clear()  # Очищуємо лише стрілки
+        self.hands_drawer.clear()
         self.draw_hand(self.hour_hand, 50, "black")
         self.draw_hand(self.minute_hand, 70, "blue")
         self.draw_hand(self.second_hand, 90, "red")
@@ -78,11 +81,33 @@ class Clock:
         self.hands_drawer.goto(0, 0)
         self.hands_drawer.pendown()
 
+
+class DigitalWatch(Watch):
+    def __init__(self, is_24_hour=True):
+        super().__init__()
+        self.is_24_hour = is_24_hour
+        self.display_drawer = turtle.Turtle()
+        self.display_drawer.hideturtle()
+
+    def display_time(self):
+        self.update_time()
+        time_str = self.current_time.strftime("%H:%M:%S") if self.is_24_hour else self.current_time.strftime(
+            "%I:%M:%S %p")
+        self.display_drawer.clear()
+        self.display_drawer.penup()
+        self.display_drawer.goto(0, -150)
+        self.display_drawer.write(time_str, align="center", font=("Arial", 16, "bold"))
+        turtle.ontimer(self.display_time, 1000)
+
+
 def main():
     turtle.tracer(0, 0)
-    clock = Clock()
-    clock.update()
+    analog_clock = AnalogWatch()
+    digital_clock = DigitalWatch(is_24_hour=False)
+    analog_clock.update()
+    digital_clock.display_time()
     turtle.mainloop()
+
 
 if __name__ == "__main__":
     main()
